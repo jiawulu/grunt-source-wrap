1b6700246873d5aeea138c8e00a853d4cb9e41e1
wuzhong <wuzhong@taobao.com>
2013-09-30 11:40:44 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,42 +16,26 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        '<%= nodeunit.tests %>'
       ],
       options: {
-        jshintrc: '.jshintrc',
+        jshintrc: '.jshintrc'
       },
     },
 
     // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp'],
+      tests: ['tmp','test/src/*__cached.js']
     },
 
     // Configuration to be run (and then tested).
     source_wrap: {
-      default_options: {
-        options: {
-        },
+     build: {
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+          'tmp': ['test/src/*','!test/src/*__cached.js']
+        }
+      }
+    }
 
   });
 
@@ -65,7 +49,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'source_wrap', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'source_wrap']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- tasks/source_wrap.js
+++ tasks/source_wrap.js
@@ -10,40 +10,27 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
+    // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('source_wrap', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
 
     // Iterate over all specified file groups.
     this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        f.src.forEach(function(filepath){
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+            var source = grunt.file.read(filepath);
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+            var compiledSource = require("./template").template(filepath,source),
+                destPath = filepath.replace(".js","__cached.js");
+
+            grunt.file.write(destPath,compiledSource);
+
+            grunt.log.warn('generate file at "' + destPath );
+
+        });
+
     });
   });
 
